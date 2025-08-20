5a78ab214502c200de51ec2badc812bc9711f29a
James Harquail <JamesRagnar@users.noreply.github.com>
2025-08-19 22:55:23 -0400

    Initial Commit

---
+++ Sources/RagnarNetworking/Socket/SocketEvent.swift
@@ -0,0 +1,17 @@
+//
+//  SocketEvent.swift
+//  RagnarNetworking
+//
+//  Created by James Harquail on 2024-12-10.
+//
+
+import Foundation
+import SocketIO
+
+public protocol SocketEvent: Sendable {
+    
+    static var name: String { get }
+    
+    associatedtype Schema: Decodable
+    
+}
