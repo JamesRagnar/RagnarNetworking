1c2b6c78d46c63bd166c7943c9243b517b21cb31
James Harquail <JamesRagnar@users.noreply.github.com>
2025-09-15 21:02:45 -0400

    Move dataTask

--- Sources/RagnarNetworking/Services/DataTaskProvider.swift
+++ Sources/RagnarNetworking/Services/DataTaskProvider.swift
@@ -13,4 +13,24 @@ public protocol DataTaskProvider {
 
 }
 
+public extension DataTaskProvider {
+    
+    static func dataTask<T: Interface>(
+        _ interface: T.Type,
+        _ parameters: T.Parameters,
+        _ configuration: ServerConfiguration,
+        _ dataTaskProvider: DataTaskProvider
+    ) async throws -> T.Response {
+        let request = try URLRequest(
+            requestParameters: parameters,
+            serverConfiguration: configuration
+        )
+        
+        return try T.handle(
+            try await dataTaskProvider.data(for: request)
+        )
+    }
+
+}
+
 extension URLSession: DataTaskProvider {}
--- Sources/RagnarNetworking/Services/RequestService.swift
+++ Sources/RagnarNetworking/Services/RequestService.swift
@@ -79,20 +79,6 @@ public extension RequestService {
 
 public extension RequestService {
     
-    static func dataTask<T: Interface>(
-        _ interface: T.Type,
-        _ parameters: T.Parameters,
-        _ configuration: ServerConfiguration,
-        _ dataTaskProvider: DataTaskProvider
-    ) async throws -> T.Response {
-        let request = try URLRequest(
-            requestParameters: parameters,
-            serverConfiguration: configuration
-        )
-        
-        return try T.handle(
-            try await dataTaskProvider.data(for: request)
-        )
-    }
+    
 
 }
