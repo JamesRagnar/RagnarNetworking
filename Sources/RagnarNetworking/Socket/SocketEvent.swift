03e5ef33063ac4e761e9605056e5ff603686509a
James Harquail <JamesRagnar@users.noreply.github.com>
2025-11-17 14:19:29 -0500

    Revert "Update sockets"
    This reverts commit 850e13f368433b3be90eb9f84590ed5d21b6d501.

--- Sources/RagnarNetworking/Socket/SocketEvent.swift
+++ Sources/RagnarNetworking/Socket/SocketEvent.swift
@@ -8,19 +8,10 @@
 import Foundation
 import SocketIO
 
-/// Protocol defining a socket event with associated types for receiving and sending
 public protocol SocketEvent: Sendable {
-
-    /// The name of the socket event
+    
     static var name: String { get }
-
-    /// The schema for receiving/decoding events
-    associatedtype Schema: Decodable & Sendable
-
-    /// The payload type for sending events (defaults to Schema)
-    associatedtype Payload: Sendable = Schema
-
-    /// The response type for acknowledgments (defaults to EmptyBody)
-    associatedtype Response: Decodable & Sendable = EmptyBody
-
+    
+    associatedtype Schema: Decodable
+    
 }
